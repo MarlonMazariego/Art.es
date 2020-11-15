a4de1c5742f34a3731b4d6d97393558521bae5cf
MarlonMazariego <61699445+MarlonMazariego@users.noreply.github.com>
2020-11-15 01:28:04 -0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -14,3 +14,16 @@
                 js.src = 'https://connect.facebook.net/es_LA/sdk/xfbml.customerchat.js';
                 fjs.parentNode.insertBefore(js, fjs);
             }(document, 'script', 'facebook-jssdk'));
+function iniciarMap(){
+    var coord = {lat: 13.6739956 ,lng: -89.2788313};
+    var map = new google.maps.Map(document.getElementById("map"),{
+        zoom: 10,
+        center: coord
+    });
+    var marker = new google.maps.Marker({
+        position: coord,
+        map: map
+    });
+}       
+            
+            
